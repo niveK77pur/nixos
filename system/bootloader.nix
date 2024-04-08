0a0e8e5eee6ee0d2b26ddf5bdcf1804e4241b201
niveK77pur <kevinbiewesch@yahoo.fr>
2024-04-08 17:16:40 +0200

    fix: use systemd-boot
    There was on issue with Grub on Titan

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -1,8 +1,14 @@
 {pkgs, ...}: {
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
+  # Bootloader GRUB.
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "/dev/nvme0n1p1";
+  # boot.loader.grub.useOSProber = true;
+
+  # Bootloader systemd-boot.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   # Kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
 }

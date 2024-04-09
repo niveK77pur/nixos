16390c9b5be8c13a083dd9742ec7e15987d6deaa
niveK77pur <kevinbiewesch@yahoo.fr>
2024-04-09 12:15:14 +0200

    feat(gaming.nix): add gaming related packages

--- topics/gaming.nix
+++ topics/gaming.nix
@@ -1,4 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   config = {
+    environment.systemPackages = with pkgs; [
+      steam
+      lutris
+      heroic
+      mangohud
+      ludusavi
+      vulkan-tools # for vkcube
+      glxinfo # for glxgears
+    ];
   };
 }

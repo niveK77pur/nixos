3143521be73ee25c17422a902b5fff01321f6719
niveK77pur <kevinbiewesch@yahoo.fr>
2024-04-12 21:55:20 +0200

    feat(media.nix): Add kde-connect
    See:
    https://www.reddit.com/r/NixOS/comments/xz4m6m/comment/irm95r8/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button

--- topics/media.nix
+++ topics/media.nix
@@ -4,7 +4,24 @@
       mpv
       yt-dlp
       ani-cli
+      kdePackages.kdeconnect-kde
       # anime4k
     ];
+
+    networking.firewall = {
+      enable = true;
+      allowedTCPPortRanges = [
+        {
+          from = 1714;
+          to = 1764;
+        } # KDE Connect
+      ];
+      allowedUDPPortRanges = [
+        {
+          from = 1714;
+          to = 1764;
+        } # KDE Connect
+      ];
+    };
   };
 }

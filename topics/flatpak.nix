ac6db103695741a91554a8d277b484d9440deb58
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-07-17 19:56:28 +0200

    refactor(flatpak): Ability to enable flatpak and necessary modules

--- topics/flatpak.nix
+++ topics/flatpak.nix
@@ -1,5 +1,15 @@
-{...}: {
-  config = {
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.flatpak;
+in {
+  options.flatpak = {
+    enable = lib.mkEnableOption "flatpak";
+  };
+
+  config = lib.mkIf cfg.enable {
     services.flatpak.enable = true;
   };
 }

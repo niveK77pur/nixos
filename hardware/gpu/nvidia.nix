f55d4922a5d22ec12b01343b0d783786ef0a0eaf
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-09-21 18:38:36 +0200

    chore(nvidia): Remove redundant 'prime-run' command over 'nvidia-offload'

--- hardware/gpu/nvidia.nix
+++ hardware/gpu/nvidia.nix
@@ -92,15 +92,6 @@ in {
         amdgpuBusId = "${cfg.hybrid.intelBusId}";
         nvidiaBusId = "${cfg.hybrid.nvidiaBusId}";
       };
-      environment.systemPackages = [
-        (pkgs.writeShellScriptBin "prime-run" ''
-          export __NV_PRIME_RENDER_OFFLOAD=1
-          export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-          export __GLX_VENDOR_LIBRARY_NAME=nvidia
-          export __VK_LAYER_NV_optimus=NVIDIA_only
-          exec "$@"
-        '')
-      ];
     })
   ]);
 }

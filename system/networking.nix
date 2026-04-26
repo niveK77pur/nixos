1a456ae9400bd2dd0e706ed9af4caecf93054bd9
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-04-26 11:05:46 +0200

    feat(system/networking): Wireless configuration can be disabled

--- system/networking.nix
+++ system/networking.nix
@@ -1,16 +1,24 @@
-{deviceName, ...}: let
+{
+  config,
+  lib,
+  ...
+}: let
   modname = "networking";
+  cfg = config.${modname};
 in {
-  options.${modname} = {};
-
-  config = {
-    networking = {
-      # Enable networking
-      wireless.iwd.enable = true;
-      networkmanager = {
-        enable = true;
-        wifi.backend = "iwd";
-      };
-    };
+  options.${modname} = {
+    enableWireless = lib.mkEnableOption "networking wireless" // {default = true;};
   };
+
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enableWireless {
+      networking = {
+        wireless.iwd.enable = true;
+        networkmanager = {
+          enable = true;
+          wifi.backend = "iwd";
+        };
+      };
+    })
+  ];
 }

f2cab9ad0883fd980a0ea550924af7eb1b7d1158
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-04-27 19:33:32 +0200

    feat(optiplex): Add Dashy

--- nixosConfigurations/optiplex.nix
+++ nixosConfigurations/optiplex.nix
@@ -35,4 +35,48 @@ in [
       };
     };
   }
+  {
+    services.dashy = {
+      enable = true;
+      virtualHost = {
+        enableNginx = true;
+        inherit domain;
+      };
+      settings = {
+        appConfig = {
+          defaultOpeningMethod = "sametab";
+          preventWriteToDisk = true;
+          preventLocalSave = true;
+          disableConfigurationForNonAdmin = true;
+          disableUpdateChecks = true;
+        };
+        pageInfo = {
+          title = "OptiPlex";
+          logo = "https://avatars.githubusercontent.com/u/10981161?v=4";
+          navLinks = [
+            {
+              title = "niveK77pur";
+              path = "https://github.com/niveK77pur";
+            }
+            {
+              title = "VinLudens GH";
+              path = "https://github.com/VinLudens";
+            }
+          ];
+        };
+        sections = [
+          {
+            name = "Services";
+            items = [
+              {
+                title = "FreshRSS";
+                icon = "${freshrss.baseUrl}/favicon.ico";
+                url = freshrss.baseUrl;
+              }
+            ];
+          }
+        ];
+      };
+    };
+  }
 ]

5c88306bb0cd186e5bd299745d7a60a5cb889edc
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-05-14 23:26:31 +0200

    feat(outputs): Specify constants for syncthing

---
+++ outputs/syncthing.nix
@@ -0,0 +1,22 @@
+{
+  devices = {
+    optiplex = {
+      id = "7PHZLSE-HMGUG2V-BL3JAMO-NGFUTFM-HFKEBSQ-SHAHSAP-RALJWPK-RG65XQS";
+      address = ["tcp://100.79.7.29:22000"];
+    };
+    supernote = {
+      id = "5O5GDWG-HIR54JQ-SFAZRVF-OIGZ6BJ-R5S56TE-WOBISJL-HTXVB6G-H26A6Q2";
+      addresses = ["tcp://100.127.82.92:22000"];
+    };
+    tuxedo = {
+      id = "QZSWKKG-XWMA7ZF-7DSR6QE-AGM4O2T-TEBBFJI-PKPT63S-MITT4FZ-QGEVTAA";
+      address = ["tcp://100.74.132.26:22000"];
+    };
+  };
+
+  folders = {
+    SN-Note.id = "8uvfz-kien7";
+    SN-MyStyle.id = "fpehy-c1mjv";
+    VinLudens-Sheets.id = "Vinludens-Sheets";
+  };
+}

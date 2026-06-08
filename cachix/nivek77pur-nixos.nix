ac73072151a94f26bc5ab26f4d08b8cc42feafa7
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-06-08 18:41:32 +0200

    chore(cachix): Add nivek77pur-nixos

---
+++ cachix/nivek77pur-nixos.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://nivek77pur-nixos.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nivek77pur-nixos.cachix.org-1:tBIn4SNnDdzl9g4NkKWMOyG4HiX7g3z7sXNhjvaib08="
+      ];
+    };
+  };
+}

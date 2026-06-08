2c925a83f00e6c363c5e78add093d008f8901ea9
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-06-08 20:47:10 +0200

    refactor(cachix): Integrate properly into config

---
+++ topics/cachix.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.cachix;
+in {
+  options.cachix = {
+    enable = lib.mkEnableOption "cachix";
+  };
+
+  config = lib.mkIf cfg.enable {
+    nix = {
+      settings = {
+        substituters = [
+          "https://nivek77pur-nixos.cachix.org"
+        ];
+        trusted-public-keys = [
+          "nivek77pur-nixos.cachix.org-1:tBIn4SNnDdzl9g4NkKWMOyG4HiX7g3z7sXNhjvaib08="
+        ];
+      };
+    };
+  };
+}
--- topics/default.nix
+++ topics/default.nix
@@ -2,9 +2,11 @@
   imports = [
     ./base.nix
     ./locate.nix
+    ./cachix.nix
   ];
 
   config = {
     base.enable = lib.mkDefault true;
+    cachix.enable = lib.mkDefault true;
   };
 }

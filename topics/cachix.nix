ca21dbc77f2074384f3b607046582329b2674309
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-06-08 20:53:29 +0200

    feat(cachix): Add noctalia cache

--- topics/cachix.nix
+++ topics/cachix.nix
@@ -14,9 +14,11 @@ in {
       settings = {
         substituters = [
           "https://nivek77pur-nixos.cachix.org"
+          "https://noctalia.cachix.org"
         ];
         trusted-public-keys = [
           "nivek77pur-nixos.cachix.org-1:tBIn4SNnDdzl9g4NkKWMOyG4HiX7g3z7sXNhjvaib08="
+          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         ];
       };
     };

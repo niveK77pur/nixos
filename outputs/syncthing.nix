0507022c871d37497a04e08d37f741d3ca76ff23
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-06-14 11:58:27 +0200

    fix(syncthing): address -> addresses typo

--- outputs/syncthing.nix
+++ outputs/syncthing.nix
@@ -2,7 +2,7 @@
   devices = {
     optiplex = {
       id = "7PHZLSE-HMGUG2V-BL3JAMO-NGFUTFM-HFKEBSQ-SHAHSAP-RALJWPK-RG65XQS";
-      address = ["tcp://100.79.7.29:22000"];
+      addresses = ["tcp://100.79.7.29:22000"];
     };
     supernote = {
       id = "5O5GDWG-HIR54JQ-SFAZRVF-OIGZ6BJ-R5S56TE-WOBISJL-HTXVB6G-H26A6Q2";
@@ -14,11 +14,11 @@
     };
     tuxedo = {
       id = "QZSWKKG-XWMA7ZF-7DSR6QE-AGM4O2T-TEBBFJI-PKPT63S-MITT4FZ-QGEVTAA";
-      address = ["tcp://100.74.132.26:22000"];
+      addresses = ["tcp://100.74.132.26:22000"];
     };
     titan = {
       id = "2QXPQSO-LHIYXWD-UVXGR6P-KFNEU5E-6SI3YAV-T47GWUI-L72R3D7-TVPRAAO";
-      address = ["tcp://100.102.201.102:22000"];
+      addresses = ["tcp://100.102.201.102:22000"];
     };
   };
 

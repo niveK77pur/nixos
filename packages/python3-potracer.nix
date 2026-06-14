83ce17d1a8ed13619d139347ba32f111db3e1d17
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-06-14 19:15:03 +0200

    feat(packages): Package pypi potracer
    Packaging inherited from an upstream PR.
    
    See: https://github.com/NixOS/nixpkgs/pull/261311

---
+++ packages/python3-potracer.nix
@@ -0,0 +1,29 @@
+# See: https://github.com/NixOS/nixpkgs/pull/261311
+{
+  lib,
+  python3,
+  fetchPypi,
+}: let
+  pname = "potracer";
+  version = "0.0.4";
+in
+  python3.pkgs.buildPythonPackage {
+    inherit pname version;
+    format = "setuptools";
+
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-MsvbmERGBmvPvotgAUKlS5D6baJ0tpIZRzIF1uTAlxM=";
+    };
+
+    propagatedBuildInputs = with python3.pkgs; [numpy];
+
+    # no upstream tests
+    doCheck = false;
+
+    meta = {
+      description = "Pure Python Port of Potrace";
+      homepage = "https://github.com/tatarize/potrace";
+      license = lib.licenses.gpl2Only;
+    };
+  }
